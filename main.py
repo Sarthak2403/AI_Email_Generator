a80249486096fa74720d94a1029510fd16973289
Sarthak Sonpatki <84634034+Sarthak2403@users.noreply.github.com>
2025-01-13 18:25:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
-
-# def create_streamlit_app(llm, portfolio, clean_text):
-#     st.title("Cold Mail Generator")
-#     url_input = st.text_input("Enter a URL:", value="https://amazon.jobs/en/jobs/2866652/software-development-engineer-ii-amazon-advertising-partner-enablement-and-growth-team-amazon-ads")
-#     submit_button = st.button("Submit")
-
-#     if submit_button:
-#         try:
-#             loader = WebBaseLoader([url_input])
-#             data = clean_text(loader.load().pop().page_content)
-#             portfolio.load_portfolio()
-#             jobs = llm.extract_jobs(data)
-#             for job in jobs:
-#                 skills = job.get('skills', [])
-#                 links = portfolio.query_links(skills)
-#                 email = llm.write_mail(job, links)
-#                 st.code(email, language='markdown')
-#         except Exception as e:
-#             st.error(f"An Error Occurred: {e}")
-
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("Cold Mail Generator")
 
